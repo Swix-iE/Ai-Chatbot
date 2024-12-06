30318db4593cffd9922b765fd95ab11084923414
Ram Mihir P. <91376295+Swix-iE@users.noreply.github.com>
2024-12-06 19:25:28 +0530

    .

--- chatbot/bot/views.py
+++ chatbot/bot/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
+# API_KEY = os.getenv("API_KEY")
+# print(API_KEY)
 def Home(request):
     return render(request, 'home.html')
